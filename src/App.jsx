3da9368d9cd4a6144ff91c9b391df0e82f19a3b0
Michael Nascimento <michael.nsc@outlook.com>
2023-03-13 11:17:24 -0300

    projeto basae

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Header } from "./components/Header";
+
+export const App = () => {
+  return (
+    <div>
+      <Header titulo="Negociações" />
+    </div>
+  );
+};
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export const Header = (props) => {
+  // Você pode modificar este arquivo, ou até apagá-lo,
+  // caso use o seu próprio projeto desenvolvido durante
+  // as primeiras semanas.
+
+  return (
+    <div className="py-5 bg-primary text-light">
+      <h1 className="text-center ">{props.titulo}</h1>
+    </div>
+  );
+};
