ba301c36fabb07de185cb957b5c9094213e0947c
Michael Nascimento <michael.nsc@outlook.com>
2023-04-02 16:46:54 -0300

    Revert "resolução"
    This reverts commit e374c11b3f9bc03b5ba0d3e8b4f70e4ec719d0d2.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
-import { useState } from "react";
-import { Formulario } from "./components/Formulario";
 import { Header } from "./components/Header";
-import { Tabela } from "./components/Tabela";
 
 export const App = () => {
-  const [negociacoes, setNegociacoes] = useState([]);
-
-  function adicionarNegociacao(negociacao) {
-    setNegociacoes([...negociacoes, negociacao]);
-  }
-
   return (
     <div>
       <Header titulo="Negociações" />
       <div className="container">
-        <Formulario quandoAdicionar={adicionarNegociacao} />
-        <Tabela negociacoes={negociacoes} />
+        {/* OS COMPONENTES SERÃO ENCAIXADOS AQUI! */}
       </div>
     </div>
   );
